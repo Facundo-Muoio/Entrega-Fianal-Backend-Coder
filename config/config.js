386ab543b056f46431e31024cab6058c2454fcd8
Facundo Muoio <facundo.muoio@gmail.com>
2022-09-01 02:09:43 -0300

    removiendo variable de entorno

--- config/config.js
+++ config/config.js
@@ -6,8 +6,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const result = dotenv.config({
   path: path.resolve(__dirname, `./enviroments/.env.${process.env.NODE_ENV}`),
 });
-const PORT = result.parsed.PORT;
-const HOST = result.parsed.HOST;
+const PORT = result.parsed.PORT || process.env.PORT;
+const HOST = result.parsed.HOST || process.env.HOST;
 const URLMONGO = result.parsed.URLMONGO
 const APP_EMAIL = result.parsed.APP_EMAIL
 const APP_EMAIL_PSW = result.parsed.APP_EMAIL_PSW
